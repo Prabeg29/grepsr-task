46c4e8d67a91c18dfc2348f22d78fb8e60491126
Prabeg Shakya <shakyaprabeg@gmail.com>
2023-04-06 22:30:46 +0545

    Task-7:Complete
    Signed-off-by: Prabeg Shakya <shakyaprabeg@gmail.com>

---
+++ task-7/task-7.php
@@ -0,0 +1,73 @@
+<?php
+
+declare(strict_types=1);
+
+class Task7 {
+  private string $_url;
+  private string $_csvFilename;
+
+  public function __construct(string $url, string $csvFilename)
+  {
+    $this->_url = $url;
+    $this->_csvFilename = $csvFilename;
+  }
+
+  public function execute(): void
+  {
+    $data = $this->getContentsFromUrl();
+    $csvContent = $this->writeToBuffer($data);
+    $this->writeToCsv($csvContent);
+  }
+
+  private function getContentsFromUrl(): array
+  {
+    $jsonData = file_get_contents($this->_url);
+
+    if(!$jsonData) {
+      return [];
+    }
+
+    return json_decode($jsonData, true);
+  }
+
+  private function writeToBuffer(array $data): string
+  {
+    if (empty($data)) {
+      throw new Exception('No content. Please provide a valid URL');
+    }
+
+    ob_start();
+
+    $buffer = fopen('php://output', 'w+');
+    fputcsv($buffer, ['Title', 'Price', 'Brand']);
+
+    foreach($data['products'] as $product) {
+      fputcsv($buffer, [
+        $product['title'] ?? 'N/A',
+        $product['price'] ?? 'N/A',
+        $product['brand'] ?? 'N/A',
+     ]);
+    }
+
+    return ob_get_clean();
+  }
+
+  private function writeToCsv(string $csvContent)
+  {
+    $file = fopen($this->_csvFilename, 'w');
+    fwrite($file, $csvContent);
+    fclose($file);
+  }
+}
+
+try {
+  $url = 'https://dummyjson.com/products/search?q=Laptop';
+  $csvFilename = 'laptop.csv';
+
+  $task7 = new Task7($url, $csvFilename);
+  $task7->execute();
+
+  echo 'Contents extracted to CSV successfully.';
+} catch (Exception $exception) {
+  var_dump($exception->getMessage());
+}
