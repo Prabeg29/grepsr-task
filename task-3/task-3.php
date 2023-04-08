938ba286fb8f3bfe1dddf58c1cf59dbd8807107d
Prabeg Shakya <shakyaprabeg@gmail.com>
2023-04-08 19:54:59 +0545

    Task-3 complete
    Signed-off-by: Prabeg Shakya <shakyaprabeg@gmail.com>

---
+++ task-3/task-3.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Write a code or function to convert the string and display date in the provided format?
+ * a. ‘October 10 2021' to '10102021’ format
+ * b. ‘1st November 2022’ to ‘01/11/2022’ format
+ */
+
+function convertDateA(string $date): string {
+  return DateTime::createFromFormat('F d Y', $date)->format('dmY');
+}
+
+function convertDateB(string $date): string {
+  return DateTime::createFromFormat('jS F Y', $date)->format('d/m/Y');
+}
+
+echo convertDateA('October 10 2021') . PHP_EOL;
+echo convertDateB('1st November 2022');
