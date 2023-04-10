2c1a502b30c92fde1be3353c3577602674895142
Prabeg Shakya <shakyaprabeg@gmail.com>
2023-04-10 09:36:19 +0545

    Refactor task-1
    Signed-off-by: Prabeg Shakya <shakyaprabeg@gmail.com>

--- task-1/task-1.php
+++ task-1/task-1.php
@@ -12,5 +12,8 @@ declare(strict_types=1);
 $arr = [2, 4, '', null, '6', '0'];
 
 print_r(
-  array_map(fn ($number): int => isset($number) && !empty($number) ? intval($number) : 9, $arr)
+  array_map(
+    fn ($element): int => isset($element) && !empty($element) && is_numeric($element) ? intval($element) : 9,
+    $arr
+  )
 );
